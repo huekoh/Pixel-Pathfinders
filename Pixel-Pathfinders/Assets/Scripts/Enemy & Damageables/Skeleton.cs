6159e67320bd6b81d4c4a151b892d85c19903a6d
Lim Win Sheng <33998278+WinSheng1@users.noreply.github.com>
2023-08-10 18:44:05 +0800

    Added items and skeleton

---
+++ Pixel-Pathfinders/Assets/Scripts/Enemy & Damageables/Skeleton.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public float damage;
+    public float knockbackForce;
+    public float moveSpeed;
+    public InventoryObject playerEquipment;
+    private float shieldProtectionValue;
+    Animator animator;
+    public DetectionZone detectionZone;
+    Rigidbody2D rb;
+    Collider2D playerCollider;
+    public bool isCollidingWithPlayer = false;
+    float damageTimer = 0f;
+    // damageInterval set to be same as Invulnerability timer
+    public float damageInterval;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        shieldProtectionValue = playerEquipment.Container.Slots[1].item.itemValue;
+    }
+
+    void FixedUpdate()
+    {
+        if (detectionZone.detectedObjects.Count > 0) {
+            // Calculate direction to player
+            Vector2 direction = (detectionZone.detectedObjects[0].transform.position - transform.position).normalized;
+
+            rb.AddForce(direction * moveSpeed * Time.deltaTime);
+
+            animator.SetFloat("Speed", moveSpeed);
+
+            if (direction.x > 0 && Math.Abs(direction.x) > Math.Abs(direction.y))
+            {
+                animator.SetFloat("Horizontal", 1);
+                animator.SetFloat("Vertical", 0);
+            } else if (direction.x < 0 && Math.Abs(direction.x) > Math.Abs(direction.y)) {
+                animator.SetFloat("Horizontal", -1);
+                animator.SetFloat("Vertical", 0);
+            } else if (direction.y > 0 && Math.Abs(direction.x) < Math.Abs(direction.y)) {
+                animator.SetFloat("Horizontal", 0);
+                animator.SetFloat("Vertical", 1);
+            } else {
+                animator.SetFloat("Horizontal", 0);
+                animator.SetFloat("Vertical", -1);
+            }
+
+        } else {
+            animator.SetFloat("Speed", 0f);
+        }
+
+        // While Slime is in contact with Player, run damageTimer to 0.5 seconds then deal damage again
+        if (isCollidingWithPlayer)
+        {
+            damageTimer += Time.deltaTime;
+            if (damageTimer > damageInterval)
+            {
+                    DealDamage();
+                    damageTimer = 0f;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // On first collision, deal damage to Player
+            DealDamage();
+            // Set collision state to true
+            isCollidingWithPlayer = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // Set collision state to false
+            isCollidingWithPlayer = false;
+        }
+    }
+
+    void DealDamage()
+    {
+        IDamageable damageable = playerCollider.GetComponent<IDamageable>();
+            
+            if (damageable != null) {
+                
+                Vector2 direction = (playerCollider.transform.position - transform.position);
+                Vector2 knockback = direction * knockbackForce;
+
+                damageable.OnHit(damage - shieldProtectionValue, knockback);
+            }
+    }
+}
